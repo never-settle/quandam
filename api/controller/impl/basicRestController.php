c80777b4949fde81ff36dd3818835017c14fb0c5
never-settle <p.notar@gmx.de>
2016-07-07 19:50:05 +0200

    restored basicrestcontroller

---
+++ api/controller/impl/basicRestController.php
@@ -0,0 +1,96 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/quandam/app/config.php";
+
+/*
+
+	Quandam API
+
+	This class provides a RESTful API interface for Quandam
+
+	Output: A formatted HTTP response
+
+	Author: Patrick Notar
+
+*/
+
+class BasicRestController {
+
+    private $httpVersion = "HTTP/1.1";
+
+    /**
+     * Sets the response code and http header to json
+     * @param int $statusCode The current HTTP status code
+     * @return array httpStatus The http status message
+     **/
+    public function setHttpHeaders($statusCode) {
+        $statusMessage = $this->getHttpStatusMessage($statusCode);
+        header($this->httpVersion . " " . $statusCode . " " . $statusMessage);
+        header("Content-Type: application/json");
+    }
+
+    /**
+     * Get the current status message
+     * @param int $statusCode The current HTTP status code
+     * @return array httpStatus The http status message
+     **/
+    public function getHttpStatusMessage($statusCode) {
+        $httpStatus = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => '(Unused)',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported');
+        return ($httpStatus[$statusCode]) ? $httpStatus[$statusCode] : $httpStatus[500];
+    }
+
+    /**
+     * Encodes the result as JSON
+     * @param array $responseData contains the response from RestController-Classes
+     * @throws Exception
+     * @return string a JSON-String
+     **/
+    public function encodeJson($responseData) {
+        if (!is_array($responseData)) throw new Exception("response data must be an array.");
+        if (is_null($responseData)) throw new Exception("response data must not be null");
+        $jsonResponse = json_encode($responseData);
+        Logger::info("deserializing response to json string: " . $jsonResponse);
+        return $jsonResponse;
+    }
+
+}
--- api/index.php
+++ api/index.php
@@ -19,18 +19,18 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
     switch ($request) {
 
 
-        // Address
+        // Ability
         // ----------------------------------------------------------------------------------------
-        case "all-addresses": {
-            $handler = new AddressRestController();
-            $handler->getAllAddresses();
+        case "all-abilities": {
+            $handler = new AbilityRestController();
+            $handler->getAllAbilities();
             break;
         }
 
-        case "address": {
-            $handler = new AddressRestController();
+        case "ability": {
+            $handler = new AbilityRestController();
             if (!$field) {
-                $handler->getAddress($_GET["id"]);
+                $handler->getAbility($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
@@ -40,18 +40,18 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
 
 
 
-        // Box
+        // Achievement
         // ----------------------------------------------------------------------------------------
-        case "all-boxes": {
-            $handler = new BoxRestController();
-            $handler->getAllBoxes();
+        case "all-achievements": {
+            $handler = new AchievementRestController();
+            $handler->getAllAchievements();
             break;
         }
 
-        case "box": {
-            $handler = new BoxRestController();
+        case "achievement": {
+            $handler = new AchievementRestController();
             if (!$field) {
-                $handler->getBox($_GET["id"]);
+                $handler->getAchievement($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
@@ -61,18 +61,18 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
 
 
 
-        // Country
+        // Goal
         // ----------------------------------------------------------------------------------------
-        case "all-countries": {
-            $handler = new CountryRestController();
-            $handler->getAllCountries();
+        case "all-goals": {
+            $handler = new GoalRestController();
+            $handler->getAllGoals();
             break;
         }
 
-        case "country": {
-            $handler = new CountryRestController();
+        case "goal": {
+            $handler = new GoalRestController();
             if (!$field) {
-                $handler->getCountry($_GET["id"]);
+                $handler->getGoal($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
@@ -82,18 +82,18 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
 
 
 
-        // Member
+        // Grade
         // ----------------------------------------------------------------------------------------
-        case "all-members": {
-            $handler = new MemberRestController();
-            $handler->getAllMembers();
+        case "all-grades": {
+            $handler = new GradeRestController();
+            $handler->getAllGrades();
             break;
         }
 
-        case "member": {
-            $handler = new MemberRestController();
+        case "grade": {
+            $handler = new GradeRestController();
             if (!$field) {
-                $handler->getMember($_GET["id"]);
+                $handler->getGrade($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
@@ -103,18 +103,18 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
 
 
 
-        // Party
+        // Level
         // ----------------------------------------------------------------------------------------
-        case "all-parties": {
-            $handler = new PartyRestController();
-            $handler->getAllParties();
+        case "all-levels": {
+            $handler = new LevelRestController();
+            $handler->getAllLevels();
             break;
         }
 
-        case "party": {
-            $handler = new PartyRestController();
+        case "level": {
+            $handler = new LevelRestController();
             if (!$field) {
-                $handler->getParty($_GET["id"]);
+                $handler->getLevel($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
@@ -124,18 +124,291 @@ if (strcmp($_SERVER["REQUEST_METHOD"], "GET") === 0) {
 
 
 
-        // Thief
+        // Mentor
         // ----------------------------------------------------------------------------------------
-        case "all-thieves": {
-            $handler = new ThiefRestController();
-            $handler->getAllThieves();
+        case "all-mentors": {
+            $handler = new MentorRestController();
+            $handler->getAllMentors();
             break;
         }
 
-        case "thief": {
-            $handler = new ThiefRestController();
+        case "mentor": {
+            $handler = new MentorRestController();
             if (!$field) {
-                $handler->getThief($_GET["id"]);
+                $handler->getMentor($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Mentorhassubject
+        // ----------------------------------------------------------------------------------------
+        case "all-mentorhassubjects": {
+            $handler = new MentorhassubjectRestController();
+            $handler->getAllMentorhassubjects();
+            break;
+        }
+
+        case "mentorhassubject": {
+            $handler = new MentorhassubjectRestController();
+            if (!$field) {
+                $handler->getMentorhassubject($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Parent
+        // ----------------------------------------------------------------------------------------
+        case "all-parents": {
+            $handler = new ParentRestController();
+            $handler->getAllParents();
+            break;
+        }
+
+        case "parent": {
+            $handler = new ParentRestController();
+            if (!$field) {
+                $handler->getParent($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Parenthasstudent
+        // ----------------------------------------------------------------------------------------
+        case "all-parenthasstudents": {
+            $handler = new ParenthasstudentRestController();
+            $handler->getAllParenthasstudents();
+            break;
+        }
+
+        case "parenthasstudent": {
+            $handler = new ParenthasstudentRestController();
+            if (!$field) {
+                $handler->getParenthasstudent($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Schoolclass
+        // ----------------------------------------------------------------------------------------
+        case "all-schoolclasses": {
+            $handler = new SchoolclassRestController();
+            $handler->getAllSchoolclasses();
+            break;
+        }
+
+        case "schoolclass": {
+            $handler = new SchoolclassRestController();
+            if (!$field) {
+                $handler->getSchoolclass($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Student
+        // ----------------------------------------------------------------------------------------
+        case "all-students": {
+            $handler = new StudentRestController();
+            $handler->getAllStudents();
+            break;
+        }
+
+        case "student": {
+            $handler = new StudentRestController();
+            if (!$field) {
+                $handler->getStudent($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Studenthasability
+        // ----------------------------------------------------------------------------------------
+        case "all-studenthasabilities": {
+            $handler = new StudenthasabilityRestController();
+            $handler->getAllStudenthasabilities();
+            break;
+        }
+
+        case "studenthasability": {
+            $handler = new StudenthasabilityRestController();
+            if (!$field) {
+                $handler->getStudenthasability($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Studenthasachievement
+        // ----------------------------------------------------------------------------------------
+        case "all-studenthasachievements": {
+            $handler = new StudenthasachievementRestController();
+            $handler->getAllStudenthasachievements();
+            break;
+        }
+
+        case "studenthasachievement": {
+            $handler = new StudenthasachievementRestController();
+            if (!$field) {
+                $handler->getStudenthasachievement($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Studenthasgoal
+        // ----------------------------------------------------------------------------------------
+        case "all-studenthasgoals": {
+            $handler = new StudenthasgoalRestController();
+            $handler->getAllStudenthasgoals();
+            break;
+        }
+
+        case "studenthasgoal": {
+            $handler = new StudenthasgoalRestController();
+            if (!$field) {
+                $handler->getStudenthasgoal($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Studenthasmentor
+        // ----------------------------------------------------------------------------------------
+        case "all-studenthasmentors": {
+            $handler = new StudenthasmentorRestController();
+            $handler->getAllStudenthasmentors();
+            break;
+        }
+
+        case "studenthasmentor": {
+            $handler = new StudenthasmentorRestController();
+            if (!$field) {
+                $handler->getStudenthasmentor($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Studenthassubject
+        // ----------------------------------------------------------------------------------------
+        case "all-studenthassubjects": {
+            $handler = new StudenthassubjectRestController();
+            $handler->getAllStudenthassubjects();
+            break;
+        }
+
+        case "studenthassubject": {
+            $handler = new StudenthassubjectRestController();
+            if (!$field) {
+                $handler->getStudenthassubject($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Subject
+        // ----------------------------------------------------------------------------------------
+        case "all-subjects": {
+            $handler = new SubjectRestController();
+            $handler->getAllSubjects();
+            break;
+        }
+
+        case "subject": {
+            $handler = new SubjectRestController();
+            if (!$field) {
+                $handler->getSubject($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // User
+        // ----------------------------------------------------------------------------------------
+        case "all-users": {
+            $handler = new UserRestController();
+            $handler->getAllUsers();
+            break;
+        }
+
+        case "user": {
+            $handler = new UserRestController();
+            if (!$field) {
+                $handler->getUser($_GET["id"]);
+                break;
+            }
+            $handler->get($field, $_GET["id"]);
+            break;
+        }
+        // ----------------------------------------------------------------------------------------
+
+
+
+        // Userhasmessage
+        // ----------------------------------------------------------------------------------------
+        case "all-userhasmessages": {
+            $handler = new UserhasmessageRestController();
+            $handler->getAllUserhasmessages();
+            break;
+        }
+
+        case "userhasmessage": {
+            $handler = new UserhasmessageRestController();
+            if (!$field) {
+                $handler->getUserhasmessage($_GET["id"]);
                 break;
             }
             $handler->get($field, $_GET["id"]);
